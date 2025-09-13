49833c071911bb708f3594da7b058fd7981c6e90
jxk0217 <308153987@qq.com>
2025-09-14 01:44:49 +0800

    feat(types): declare basic types

--- include/ring/core/types.h
+++ include/ring/core/types.h
@@ -3,7 +3,7 @@
 
 #include <cstdint>
 
-namespace ring::types
+namespace ring::core
 {
 
 using int8  = std::int8_t;
@@ -16,32 +16,32 @@ using uint16    = std::uint16_t;
 using uint32    = std::uint32_t;
 using uint64    = std::uint64_t;
 
-} // namespace ring::types
+} // namespace ring::core
 
 #if !defined(int8)
-    using int8  = ring::types::int8;
+    using int8  = ring::core::int8;
 #endif
 #if !defined(int16)
-    using int16 = ring::types::int16;
+    using int16 = ring::core::int16;
 #endif
 #if !defined(int32)
-    using int32 = ring::types::int32;
+    using int32 = ring::core::int32;
 #endif
 #if !defined(int64)
-    using int64 = ring::types::int64;
+    using int64 = ring::core::int64;
 #endif
 
 #if !defined(uint8)
-    using uint8     = ring::types::uint8;
+    using uint8     = ring::core::uint8;
 #endif
 #if !defined(uint16)
-    using uint16    = ring::types::uint16;
+    using uint16    = ring::core::uint16;
 #endif
 #if !defined(uint32)
-    using uint32    = ring::types::uint32;
+    using uint32    = ring::core::uint32;
 #endif
 #if !defined(uint64)
-    using uint64    = ring::types::uint64;
+    using uint64    = ring::core::uint64;
 #endif
 
 #endif // !__RING_CORE_TYPES_H__
